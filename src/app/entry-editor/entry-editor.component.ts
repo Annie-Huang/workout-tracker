3f1151adf7c0ee1a8dbd9ecc6257976c9e768b87
Annie Huang <annie.hn@gmail.com>
2018-12-27 20:39:13 +1100

    Add a simple ngbTypeahead

--- src/app/entry-editor/entry-editor.component.ts
+++ src/app/entry-editor/entry-editor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { WorkoutsApiService } from '../services/workouts-api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import {NgbDate, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-entry-editor',
@@ -13,6 +15,7 @@ export class EntryEditorComponent implements OnInit {
   public loading = false;
   public startDate: any;
   public maxDate: NgbDateStruct;
+  public locations = ['Main gym', 'Home gym', 'Neighborhood 1 mile course', 'Neighborhood 3 mile course', 'Neighborhood 5 mile course', 'Smallville 20 mile course', 'Smallville 25 mile course', 'Smallville 30 mile course'];
 
   constructor(private router: ActivatedRoute, private nav: Router, private api: WorkoutsApiService) {
     const today = new Date();
@@ -32,6 +35,14 @@ export class EntryEditorComponent implements OnInit {
       }
     });
   }
+
+  locationsSearch = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),  // wait for user to stop typing for 200 ms.
+      distinctUntilChanged(),
+      map(term => term.length < 2 ? []
+        : this.locations.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    );
 
   save() {
     this.loading = true;
