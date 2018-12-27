b5211a37c56c973ed2ad6892ba68388cbe69e747
Annie Huang <annie.hn@gmail.com>
2018-12-28 09:51:49 +1100

    Set Images to use variable and loop through

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  public images = [
+    { src: 'assets/images/bike-1.png', title: 'Bike 1' },
+    { src: 'assets/images/row-1.png', title: 'Row 1' },
+    { src: 'assets/images/row-2.png', title: 'Row 2' },
+    { src: 'assets/images/run-1.png', title: 'Run 1' }
+  ];
 
   constructor() { }
 
